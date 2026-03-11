b5ae5f27b501a5d6708464224553fceda3887f70
Petr Homola <homola.p@gmail.com>
2026-03-11 11:36:33 +0000

    support for slices

--- jsonschema/schema_test.go
+++ jsonschema/schema_test.go
@@ -12,19 +12,23 @@ import (
 func TestSchemaInference(t *testing.T) {
 	req := require.New(t)
 
-	type s struct {
-		Text   string  `json:"text"`
-		Num1   int     `json:"num1"`
-		Num2   float32 `json:"num2"`
-		Num3   float64 `json:"num3"`
-		Check  bool    `json:"check"`
-		Inner1 struct {
+	type (
+		named struct {
 			Name string `json:"name" jsonschema:"Some name."`
-		} `json:"inner1"`
-		Inner2 *struct {
-			Name string `json:"name,omitempty"`
-		} `json:"inner2"`
-	}
+		}
+
+		s struct {
+			Text   string   `json:"text"`
+			Num1   int      `json:"num1"`
+			Num2   float32  `json:"num2"`
+			Num3   float64  `json:"num3"`
+			Check  bool     `json:"check"`
+			Inner1 named    `json:"inner1"`
+			Inner2 *named   `json:"inner2"`
+			List1  []string `json:"list1"`
+			List2  []named  `json:"list2"`
+		}
+	)
 
 	sch, err := For[s]()
 	req.Nil(err)
